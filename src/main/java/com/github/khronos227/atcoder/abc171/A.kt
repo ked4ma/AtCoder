d55e5b8a06dd1ef7c7083be829b3d0f3042e74c8
takefumi.ota <takefumi@thecoo.co.jp>
2020-06-21 22:53:40 +0900

    abc171

---
+++ src/main/java/com/github/khronos227/atcoder/abc171/A.kt
@@ -0,0 +1,24 @@
+package com.github.khronos227.atcoder.abc171
+
+fun main() {
+    val alpha = next()[0].toInt()
+    if (alpha in (65..90)) {
+        println("A")
+    } else {
+        println("a")
+    }
+}
+
+// # Utils
+// ## Input
+fun next() = readLine()!!
+fun nextInt() = next().toInt()
+fun nextLong() = next().toLong()
+fun nextDouble() = next().toDouble()
+fun nextList() = next().split(" ")
+fun nextIntList() = next().split(" ").map(String::toInt)
+fun nextLongList() = next().split(" ").map(String::toLong)
+fun nextDoubleList() = next().split(" ").map(String::toDouble)
+
+// ## Array
+inline fun <reified T> sizedArray(size: Int, default: T) = Array(size) { default }
---
+++ src/main/java/com/github/khronos227/atcoder/abc171/C.kt
@@ -0,0 +1,18 @@
+package com.github.khronos227.atcoder.abc171
+
+import java.lang.StringBuilder
+
+fun main() {
+    var n = nextLong()
+    val res = StringBuilder()
+    while (n > 0) {
+        var m = n % 26
+        n /= 26
+        if (m == 0L) {
+            m = 26
+            n--
+        }
+        res.insert(0, m.plus(96).toChar())
+    }
+    println(res.toString())
+}
