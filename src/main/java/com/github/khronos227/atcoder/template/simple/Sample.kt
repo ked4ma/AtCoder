3f7cbb88ae39b93eae7c03824dd4dc07284c18c1
takefumi.ota <takefumi@thecoo.co.jp>
2020-06-27 15:44:37 +0900

    add modinv func

--- src/main/java/com/github/khronos227/atcoder/template/simple/Sample.kt
+++ src/main/java/com/github/khronos227/atcoder/template/simple/Sample.kt
@@ -21,3 +21,22 @@ fun nextDoubleList() = next().split(" ").map(String::toDouble)
 
 // ## Array
 inline fun <reified T> sizedArray(size: Int, default: T) = Array(size) { default }
+
+// ## mod^-1
+//    [NOTE] CANNOT use this for 0, mod, 2mod, 3mod ...(return wrong value)
+fun modinv(num: Long, mod: Long): Long {
+    var a = num
+    var b = mod
+    var u = 1L
+    var v = 0L
+    while (b > 0) {
+        val t = a.div(b)
+        a -= t * b
+        a = b.also { b = a } // swap a and b
+        u -= t * v
+        u = v.also { v = u } // swap u and v
+    }
+    u %= mod
+    if (u < 0) u += mod
+    return u
+}
