e03deba13cc5002bdf6c62bb018bd3c11d2821b7
takefumi ota <takefumi.ota@MacBook-Pro.local>
2020-10-11 17:54:36 +0900

    hhkb2020

---
+++ src/main/java/com/github/khronos227/atcoder/hhkb2020/C.kt
@@ -0,0 +1,15 @@
+package com.github.khronos227.atcoder.hhkb2020
+
+fun main() {
+    val n = nextInt()
+    val p = nextIntList()
+    val memo = mutableSetOf<Int>()
+    var v = 0
+    repeat(n) {
+        memo.add(p[it])
+        while (v in memo) {
+            v++
+        }
+        println(v)
+    }
+}
