85802c7f6d34ceaf4a9ad9de54aad46ff85659e4
takefumi ota <takefumi.ota@MacBook-Pro.local>
2020-10-24 17:35:22 +0900

    abc051_na

---
+++ src/main/java/com/github/khronos227/atcoder/abc051_na/B.kt
@@ -0,0 +1,16 @@
+package com.github.khronos227.atcoder.abc051_na
+
+fun main() {
+    val (k, s) = nextIntList()
+    var count = 0
+    x@ for (x in (0..k)) {
+        y@ for (y in (0..k)) {
+            val z = s - x - y
+            if (z < 0) break@y
+            if (z in (0..k)) {
+                count++
+            }
+        }
+    }
+    println(count)
+}
---
+++ src/main/java/com/github/khronos227/atcoder/abc051_na/C.kt
@@ -0,0 +1,45 @@
+package com.github.khronos227.atcoder.abc051_na
+
+fun main() {
+    val (sx, sy, tx, ty) = nextIntList()
+    var res = ""
+    // start with y axis, cuz sample answer started with it.
+    // (sx, sy) -> (sx, ty)
+    repeat(ty - sy) {
+        res += "U"
+    }
+    // (sx, ty) -> (tx, ty)
+    repeat(tx - sx) {
+        res += "R"
+    }
+    // (tx, ty) -> (tx, sy)
+    repeat(ty - sy) {
+        res += "D"
+    }
+    // (tx, sy) -> (sx - 1, sy)
+    repeat(tx - sx + 1) {
+        res += "L"
+    }
+    // 2round
+    // (sx - 1, sy) -> (sx - 1, ty + 1)
+    repeat(ty - sy + 1) {
+        res += "U"
+    }
+    // (sx - 1, ty + 1) -> (tx, ty + 1)
+    repeat(tx - sx + 1) {
+        res += "R"
+    }
+    //  (tx, ty + 1) -> (tx, ty) -> (tx + 1, ty)
+    res += "DR"
+    // (tx + 1, ty) -> (tx + 1, sy - 1)
+    repeat(ty - sy + 1) {
+        res += "D"
+    }
+    // (tx + 1, sy - 1) -> (sx , sy - 1)
+    repeat(tx - sx + 1) {
+        res += "L"
+    }
+    // (sx, sy - 1) -> (sx, sy)
+    res += "U"
+    println(res)
+}
