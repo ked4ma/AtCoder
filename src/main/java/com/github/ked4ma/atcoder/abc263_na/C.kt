a71785dd73eae1d2a7d41c987b8366a15b916a72
ked4ma <hades.netherworld@gmail.com>
2022-08-13 17:10:17 +0900

    abc263_na

---
+++ src/main/java/com/github/ked4ma/atcoder/abc263_na/C.kt
@@ -0,0 +1,19 @@
+package com.github.ked4ma.atcoder.abc263_na
+
+import com.github.ked4ma.atcoder.utils.*
+
+@OptIn(ExperimentalStdlibApi::class)
+fun main() {
+    val (N, M) = nextIntList()
+    var list = (1..M).map { listOf(it) }
+    repeat(N - 1) {
+        list = buildList {
+            list.forEach { elems ->
+                for (i in elems.last() + 1..M) {
+                    add(elems + i)
+                }
+            }
+        }
+    }
+    list.forEach { println(it.joinToString(separator = " ")) }
+}
