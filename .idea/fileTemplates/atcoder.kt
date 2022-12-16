3b6d90c98d15bb66e6cf09abe58a9c8cb08ab463
ked4ma <hades.netherworld@gmail.com>
2022-12-16 14:29:27 +0900

    update .idea files

---
+++ .idea/fileTemplates/atcoder.kt
@@ -0,0 +1,13 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
+
+#end
+#parse("File Header.java")
+import com.github.ked4ma.atcoder.utils.*
+
+fun main() {
+    println("This is template.")
+    println("1: Implement solution")
+    println("2: ./gradlew run")
+    println("     or")
+    println("   ./gradlew run < [path/to/input.txt]")
+}
