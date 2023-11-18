da08206b1eb20f22a5d6f6cfe98eab346930cb42
ked4ma <hades.netherworld@gmail.com>
2023-11-18 23:23:52 +0900

    abc329

---
+++ src/main/java/com/github/ked4ma/atcoder/abc329/E_x.kt
@@ -0,0 +1,35 @@
+package com.github.ked4ma.atcoder.abc329
+
+import com.github.ked4ma.atcoder.utils.array.*
+import com.github.ked4ma.atcoder.utils.input.default.*
+
+// make run <TASK: A/B/...> [BRANCH=feature/<CONTEST: abc000>]
+fun main() {
+    val (N, M) = nextIntList()
+    val S = next().toCharArray()
+    val T = next().toCharArray()
+    val queue = ArrayDeque<Int>()
+    val used = sizedArray(N + 1, false)
+    fun check(index: Int) {
+        if (used[index]) return
+        val ok = (0 until M).all { i -> S[index + i] == '#' || S[index + i] == T[i] }
+        if (ok) {
+            used[index] = true
+            queue.addLast(index)
+        }
+    }
+    // init
+    for (i in 0..N - M) {
+        check(i)
+    }
+    while (queue.isNotEmpty()) {
+        val index = queue.removeFirst()
+        for (i in 0 until M) {
+            S[index + i] = '#'
+        }
+        for (i in (index - M + 1).coerceAtLeast(0)..(index + M - 1).coerceAtMost(N - M)) {
+            check(i)
+        }
+    }
+    println(if (S.all { it == '#' }) "Yes" else "No")
+}
