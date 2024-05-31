a9c597f4b0c74505e9726bf7dcf0cf73c195fb02
ked4ma <hades.netherworld@gmail.com>
2024-06-01 01:00:32 +0900

    update Utils

--- src/main/kotlin/com/github/ked4ma/atcoder/utils/number/int/bit/Code.kt
+++ src/main/kotlin/com/github/ked4ma/atcoder/utils/number/int/bit/Code.kt
@@ -1,6 +1,14 @@
 package com.github.ked4ma.atcoder.utils.number.int.bit
 
 infix fun Int.bit(i: Int) = (this shr i) and 1 == 1
+
+/**
+ * get bit length to represent number.
+ * e.g.
+ *   4: 0x100 = 3
+ *   5: 0x101 = 3
+ *   1: 0x1 = 1
+ */
 fun Int.bitLength(): Int {
     if (this == 0) return 0
     var n = this
